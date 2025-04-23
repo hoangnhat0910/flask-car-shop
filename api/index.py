e1cf44c1d200853ad8596d7bc4800cb0781a632c
nhathoang0910 <hoangnhat0910@gmail.com>
2025-04-24 04:34:45 +0700

    Successfully configured to upload to vercel.

--- api/index.py
+++ api/index.py
@@ -2,5 +2,5 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from shop import app as application  # Flask app entry point
+from shop import app
 
